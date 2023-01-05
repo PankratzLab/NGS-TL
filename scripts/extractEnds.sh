f30d13fd6321f34e879bf12d92bac4344a242aa5
jlanej <jlanej@gmail.com>
2023-01-05 17:25:31 -0600

    :floppy_disk:

--- scripts/extractEnds.sh
+++ scripts/extractEnds.sh
@@ -21,7 +21,7 @@ regionsSearch=$5
 
 echo "extracting regions defined by $regionsSearch from $cramFile to $outCram"
 # If we do not add the perl portion, -X to specify the crai gets angry when reading from amazon or google buckets.
-# Typically samtools is able to extract regions defined by a bed file, but doesn't like to do so from cloud sources
+# Typically samtools is able to extract regions defined by a bed file, but doesn't like to do so from cloud sources (or I had specified something incorrectly)
 
 samtools view -b -o "$outCram" -X --reference $ref -M -L "$regionsSearch" -h "$cramFile" "$craiFile" $(perl -ane '{print "$F[0]:$F[1]-$F[2] "}' $regionsSearch )
 
