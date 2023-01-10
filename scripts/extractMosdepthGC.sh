70f9869a31095664e6c37f906b63e82559af8d3c
jlanej <jlanej@gmail.com>
2023-01-10 15:21:26 -0600

    refactor to clean HQ GC

--- scripts/extractMosdepthGC.sh
+++ scripts/extractMosdepthGC.sh
@@ -7,33 +7,11 @@ gcBedFile=$3
 gcBedIndexFile=$4
 outputFile=$5
 
-vals = as.numeric(readLines ("stdin"))
-data.frame(
-  min = min(vals),
-  max = max(vals),
-  mean = mean(vals),
-  sd = sd(vals),
-  median = median(vals),
-  mad = mad(vals),
-  iqr = IQR(vals),
-  n = n(),
-)
 
+gcBedFile=/home/pankrat2/public/bin/ref/GRCh38_full_analysis_set_plus_decoy_hla.1kb.LTL.GC.filtered.bed.gz
+mosdepthFile=/scratch.global/lanej/topMed/mosdepth.all/data/mosdepth/broad/WHI/NWD883937/NWD883937.by1000.regions.bed.gz
 
-
-bedtools intersect -a $mosdepthFile -b $gcBedFile | cut -f4 |Rscript $rSummary |gzip>$outputFile
-
-
-library(dplyr)
-vals = as.numeric(readLines ("stdin"))
-vals = as.data.frame(vals)
-vals %>% summarise(
-  min = min(vals),
-  max = max(vals),
-  mean = mean(vals),
-  sd = sd(vals),
-  median = median(vals),
-  mad = mad(vals),
-  iqr = IQR(vals),
-  n = n(),
-)
+# ;d=data.frame(min = min(vals),max = max(vals),mean = mean(vals),sd = sd(vals),median = median(vals),mad = mad(vals),iqr = IQR(vals),n = length(vals))
+summarizeScript='vals = as.numeric(readLines ("stdin"))'
+bedtools intersect -a $mosdepthFile -b $gcBedFile | cut -f4 |Rscript -e 'vals = as.numeric(readLines ("stdin"));data.frame(min = min(vals),max = max(vals),mean = mean(vals),sd = sd(vals),median = median(vals),mad = mad(vals),iqr = IQR(vals),n = length(vals))' \
+|gzip>$outputFile
