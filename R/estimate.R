2529cc19380ea1a64efc7328230c743fc76052be
jlanej <jlanej@gmail.com>
2023-01-11 14:40:51 -0600

    consolidated out

--- R/estimate.R
+++ R/estimate.R
@@ -1,6 +1,4 @@
 args = commandArgs(trailingOnly = TRUE)
-library(sumsamstats)
-
 # tlCountFile=args[[1]]
 # gcStatsFile=args[[2]]
 # samtoolsStatsFile=args[[3]]
@@ -14,20 +12,24 @@ kmax = 25
 
 tlCounts = read.delim(tlCountFile)
 baseStats = read.delim(gcStatsFile, sep = "")
+colnames(baseStats) = paste0("mosdepth_gc_coverage_", colnames(baseStats))
 stats = readLines(samtoolsStatsFile)
 baseStats$READ_LENGTH = as.numeric(strsplit(stats[grep(pattern = "maximum length", stats)], split = "\t")[[1]][[3]])
 
 
 
 computeTLInternalCountMD <- function(tl, baseStats, k) {
-  telCount = sum(tl[which(tl$RepeatK >= k),]$Count)
-  gcCount = (baseStats$mean * 1000 * baseStats$n) / baseStats$READ_LENGTH
+  telCount = sum(tl[which(tl$RepeatK >= k), ]$Count)
+  gcCount = (baseStats$mosdepth_gc_coverage_mean * 1000 * baseStats$mosdepth_gc_coverage_n) / baseStats$READ_LENGTH
   rat = telCount / gcCount
   scale = 332720800 / 1000 / 46
   tl = rat * scale
-  return(tl)
+  resultTmp = data.frame(k = k)
+  resultTmp[, paste0("TL_READS_AT_K_", k)] = telCount
+  resultTmp[, paste0("LENGTH_ESTIMATE_AT_K_", k)] = tl
+  resultTmp$k = NULL
+  return(resultTmp)
 }
-
 
 ks = c(1:kmax)
 
@@ -37,6 +39,6 @@ for (k in ks) {
                                       baseStats = baseStats,
                                       k = k)
   
-  results[, paste0("LENGTH_ESTIMATE_AT_K_", k)] =   estimate
+  results = cbind(results, estimate)
 }
 print(results)
