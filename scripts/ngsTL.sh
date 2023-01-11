41629b222c5ad8a9fba02f6231d6fbd4ea77f7ea
jlanej <jlanej@gmail.com>
2023-01-11 16:04:11 -0600

    :floppy_disk:

--- scripts/ngsTL.sh
+++ scripts/ngsTL.sh
@@ -3,9 +3,17 @@
 # Estimate TL from a bam or cram file
 
 echo $0
+repoDirectory=$(dirname $0)
+echo $repoDirectory
+
 cramFile=$1
 craiFile=$2
 outCram=$3
 ref=$4
 regionsSearch=$5
 rootOut=$6
+mosdepthFile=$1
+mosdepthIndexFile=$2
+gcBedFile=$3
+gcBedIndexFile=$4
+outputFile=$5
