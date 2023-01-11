8f9f81334559f24e43df54d9e713a4f0bde28c40
jlanej <jlanej@gmail.com>
2023-01-11 13:54:20 -0600

    parse readlength

--- R/estimate.R
+++ R/estimate.R
@@ -1,14 +1,22 @@
 args = commandArgs(trailingOnly = TRUE)
+library(sumsamstats)
 
 # tlCountFile=args[[1]]
 # gcStatsFile=args[[2]]
-k = args[[3]]
+# samtoolsStatsFile=args[[3]]
+# kmax=args[[4]]
+
 tlCountFile = "/Users/Kitty/tmp/LTL_tests/NWD883937.ltl.counts.txt.gz"
 gcStatsFile = "/Users/Kitty/tmp/LTL_tests/NWD883937.gc.stats.txt.gz"
-k = 9
+samtoolsStatsFile = "/Users/Kitty/tmp/LTL_tests/NWD883937.stats.txt.gz"
+kmax = 25
 
 
 tlCounts = read.delim(tlCountFile)
+gcStats = read.delim(gcStatsFile)
+stats = readLines(samtoolsStatsFile)
+readLength = as.numeric(strsplit(stats[grep(pattern = "maximum length", stats)], split = "\t")[[1]][[3]])
+
 
 computeTLInternalCountMD <- function(df, col, normCol) {
   telCounts = df[, col]
