9bf011784ca3985367477bbaa4807420b7844858
jlanej <jlanej@gmail.com>
2023-01-11 11:04:53 -0600

    prep example LTL computation

--- scripts/countTLReads.sh
+++ scripts/countTLReads.sh
@@ -2,16 +2,24 @@
 
 # Count telomeric content in reads
 
-cramFile=$1
-craiFile=$2
+bamOrCram=$1
+bamOrCramIndex=$2
 outputFile=$3
 
-sample=$(samtools samples $cramFile)
+
+
+bamOrCram=/scratch.global/lanej/topMed/LTL/experimental/bams_BDC_LTL_V2/NWD883937.b38.irc.v1.LTL.bam
+outputFile=/home/tsaim/lane0212/tmp/LTL_tests/NWD883937.ltl.counts.txt.gz
+
+
+sample=$(samtools samples $bamOrCram)
 
 awk -v OFS='\t' 'BEGIN {print "Count","RepeatK","Sample","Cram"}' |gzip> $outputFile
 
 
-samtools view -h $cramFile \
+
+
+samtools view -h $bamOrCram \
 | awk -v OFS='\t'  -v readIndex=10 \
 '{k1=gsub(/CCCTAA/, "CCCTAA",$readIndex);k2=gsub(/TTAGGG/, "TTAGGG",$readIndex)}{if(k1 >= k2){print k1} else {print k2}}' \
 |sort |uniq -c |awk -v sample=$sample -v OFS='\t' -v IFS=' ' '{print $1,$2,sample}' |gzip >> $outputFile
--- scripts/estimate.sh
+++ scripts/estimate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# summary stats for mosdepth bins overlapping a bed file
+# estimates LTL using mosdepth median GC content
 mosdepthFile=$1
 mosdepthIndexFile=$2
 gcBedFile=$3
@@ -8,11 +8,8 @@ gcBedIndexFile=$4
 outputFile=$5
 
 
-module load bedtools
-module load R
-# gcBedFile=/home/pankrat2/public/bin/ref/GRCh38_full_analysis_set_plus_decoy_hla.1kb.LTL.GC.filtered.bed.gz
-# mosdepthFile=/scratch.global/lanej/topMed/mosdepth.all/data/mosdepth/broad/WHI/NWD883937/NWD883937.by1000.regions.bed.gz
-# outputFile=/home/tsaim/lane0212/tmp/LTL_tests/NWD883937.gc.stats.txt.gz
+gcBedFile=/home/pankrat2/public/bin/ref/GRCh38_full_analysis_set_plus_decoy_hla.1kb.LTL.GC.filtered.bed.gz
+mosdepthFile=/scratch.global/lanej/topMed/mosdepth.all/data/mosdepth/broad/WHI/NWD883937/NWD883937.by1000.regions.bed.gz
 
 summarizeScript='vals = as.numeric(readLines ("stdin"))'
 bedtools intersect -a $mosdepthFile -b $gcBedFile | cut -f4 |Rscript -e 'vals = as.numeric(readLines ("stdin"));data.frame(min = min(vals),max = max(vals),mean = mean(vals),sd = sd(vals),median = median(vals),mad = mad(vals),iqr = IQR(vals),n = length(vals))' \
