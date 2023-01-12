676e42c1b5e65ab04e8d28ac3c0e0557e9fe9f77
jlanej <jlanej@gmail.com>
2023-01-12 13:08:46 -0600

    :floppy_disk:

--- scripts/ngsTL.sh
+++ scripts/ngsTL.sh
@@ -7,14 +7,11 @@ repoDirectory=$(dirname $0)
 echo $repoDirectory
 
 cramFile=$1
-craiFile=$2
-outCram=$3
-ref=$4
-regionsSearch=$5
-rootOut=$6
-mosdepthFile=$1
-mosdepthIndexFile=$2
-gcBedFile=$3
-gcBedIndexFile=$4
-outputFile=$5
+rootOut=$2
+ref=$3
+regionsSearch=$4
+gcBedFile=$5
 
+
+
+$repoDirectory/mosdepth.sh $cramFile $rootOut $ref
