4fdc64321aeb8164193c4cf5900766c42e1e7dec
jlanej <jlanej@gmail.com>
2023-01-12 13:55:34 -0600

    trim GC args

--- scripts/extractMosdepthGC.sh
+++ scripts/extractMosdepthGC.sh
@@ -3,11 +3,8 @@
 # summary stats for mosdepth bins overlapping a bed file. For TL, these are HQ bins containing ~similar GC content as telomeric repeats
 
 mosdepthFile=$1
-mosdepthIndexFile=$2
-gcBedFile=$3
-gcBedIndexFile=$4
-outputFile=$5
+gcBedFile=$2
+outputFile=$3
 
-summarizeScript='vals = as.numeric(readLines ("stdin"))'
 bedtools intersect -a $mosdepthFile -b $gcBedFile | cut -f4 |Rscript -e 'vals = as.numeric(readLines ("stdin"));data.frame(min = min(vals),max = max(vals),mean = mean(vals),sd = sd(vals),median = median(vals),mad = mad(vals),iqr = IQR(vals),n = length(vals))' \
 |gzip>$outputFile
--- scripts/ngsTL.sh
+++ scripts/ngsTL.sh
@@ -7,11 +7,20 @@ repoDirectory=$(dirname $0)
 echo $repoDirectory
 
 cramFile=$1
+# cramIndexFile: required
 rootOut=$2
 ref=$3
+# ref*: needs indx
 regionsSearch=$4
 gcBedFile=$5
 
 
 
 $repoDirectory/mosdepth.sh $cramFile $rootOut $ref
+
+# required output 
+mosdepthFile=$rootOut.regions.bed.gz
+mosdepthIndexFile=$rootOut.regions.bed.csi
+
+$repoDirectory/extractMosdepthGC.sh $mosdepthFile $mosdepthIndexFile $gcBedFile $rootOut.ltl.gz.bed.gz
+
