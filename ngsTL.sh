23107588e041b0e5ed890b8820e6b5f78e24ce25
jlanej <jlanej@gmail.com>
2023-02-07 15:02:25 -0600

    full opts

--- ngsTL.sh
+++ ngsTL.sh
@@ -2,7 +2,7 @@
 
 # Estimate TL from a bam or cram file
 
-options=$(getopt -l "cramFile:,craiFile:,rootOut:" -o "" -a -- "$@")
+options=$(getopt -l "cramFile:,craiFile:,rootOutput:referenceGenome:" -o "" -a -- "$@")
 
 eval set -- "$options"
 
@@ -23,10 +23,33 @@ do
             shift
             export craiFile="$1"
         ;;
-        # The root output path (e.g. rootOut=/path/to/example should result in the creation of /path/to/example.ltl.estimate.txt.gz)
-        --rootOut)
+        # The root output path (e.g. rootOutput=/path/to/example should result in the creation of /path/to/example.ltl.estimate.txt.gz)
+        --rootOutput)
             shift
-            export rootOut="$1"
+            export rootOutput="$1"
+        ;;
+        # The reference genome used to align the cram or bam (e.g. GRCh38_full_analysis_set_plus_decoy_hla.fa)
+        --referenceGenome)
+            shift
+            export referenceGenome="$1"
+        ;;
+        # Stores 1kb bins that have gc content similar to telomeric repeats.
+        # Unless you want to change it, this can be set to https://github.com/PankratzLab/NGS-TL/blob/main/resources/GRCh38_full_analysis_set_plus_decoy_hla.1kb.LTL.GC.filtered.bed.gz
+        --gcBedFile)
+            shift
+            export gcBedFile="$1"
+        ;;
+        # A bed file defining the regions to look for telomeric reads (typically 25kb from the ends of each chromosome)
+        # Unless you want to change it, this can be set to https://github.com/PankratzLab/NGS-TL/blob/main/resources/25kb.bins.bed
+        --regionsSearch)
+            shift
+            export regionsSearch="$1"
+        ;;
+        # (Optional) If a mosdepth file is provided, mosdepth will not be run.
+        # If mosdepth file is not provided, mosdepth will be run to produce $rootOutput.regions.bed.gz
+        --mosdepthFile)
+            shift
+            export mosdepthFile="$1"
         ;;
         --)
             shift
@@ -41,26 +64,17 @@ echo "repo directory: $repoDirectory"
 
 echo "cram: $cramFile"
 echo "crai: $craiFile"
-echo "root output: $rootOut"
+echo "root output: $rootOutput"
+mkdir -p $(dirname $rootOutput)
+
+echo "referenceGenome: $referenceGenome"
+echo "gcBedFile: $gcBedFile"
+echo "regionsSearch: $regionsSearch"
+echo "mosdepthFile: $mosdepthFile"
+
+
 
 exit 0
-
-# # cramFile=$1
-# craiFile=$2
-# rootOut=$3
-
-# Stores 1kb bins that have gc content similar to telomeric repeats.
-# Unless you want to change it, this can be set to https://github.com/PankratzLab/NGS-TL/blob/main/resources/GRCh38_full_analysis_set_plus_decoy_hla.1kb.LTL.GC.filtered.bed.gz
-gcBedFile=$4
-# The reference genome used to align the cram or bam (e.g. GRCh38_full_analysis_set_plus_decoy_hla.fa)
-ref=$5
-# A bed file defining the regions to look for telomeric reads (typically 25kb from the ends of each chromosome)
-# Unless you want to change it, this can be set to https://github.com/PankratzLab/NGS-TL/blob/main/resources/25kb.bins.bed
-regionsSearch=$6
-# (Optional) If a mosdepth file is provided, mosdepth will not be run.
-# If mosdepth file is not provided, mosdepth will be run to produce $rootOut.regions.bed.gz
-mosdepthFile=$7
-
 
 
 
@@ -68,31 +82,31 @@ mosdepthFile=$7
 if [ -z "$mosdepthFile" ]
 then
     # output file of interest
-    mosdepthFile=$rootOut.regions.bed.gz
+    mosdepthFile=$rootOutput.regions.bed.gz
     
     echo "Running mosdepth task"
-    [ -f "$mosdepthFile" ] || $repoDirectory/scripts/mosdepth.sh "$cramFile" "$rootOut" "$ref"
+    [ -f "$mosdepthFile" ] || $repoDirectory/scripts/mosdepth.sh "$cramFile" "$rootOutput" "$referenceGenome"
     
 else echo "mosdepthFile is set to '$mosdepthFile'"
 fi
 
 # output file of interest
-gcStatsFile="$rootOut.ltl.gc.stats.txt.gz"
+gcStatsFile="$rootOutput.ltl.gc.stats.txt.gz"
 echo "extracting GC regions task"
 [ -f "$gcStatsFile" ] || $repoDirectory/scripts/extractMosdepthGC.sh "$mosdepthFile" "$gcBedFile" "$gcStatsFile"
 
 # output file of interest
-outTLCram="$rootOut.ltl.cram"
+outTLCram="$rootOutput.ltl.cram"
 samtoolsStatsFile="$outTLCram".stats.txt.gz
 echo "extracting ends task"
-[ -f "$outTLCram" ] || $repoDirectory/scripts/extractEnds.sh "$cramFile" "$craiFile" "$outTLCram" "$ref" "$regionsSearch"
+[ -f "$outTLCram" ] || $repoDirectory/scripts/extractEnds.sh "$cramFile" "$craiFile" "$outTLCram" "$referenceGenome" "$regionsSearch"
 
 # output file of interest
-tlCountFile="$rootOut.ltl.counts.txt.gz"
+tlCountFile="$rootOutput.ltl.counts.txt.gz"
 echo "counting tl reads task"
 [ -f "$tlCountFile" ] || $repoDirectory/scripts/countTLReads.sh $outTLCram $tlCountFile
 
 # output file of interest
-outTLEstimate="$rootOut.ltl.estimate.txt.gz"
+outTLEstimate="$rootOutput.ltl.estimate.txt.gz"
 echo "estimating tl task"
 $repoDirectory/scripts/estimateTL.sh $tlCountFile $gcStatsFile $samtoolsStatsFile $outTLEstimate
