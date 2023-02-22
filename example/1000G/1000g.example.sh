f6706fdc64142da3d271f91d668107bca5fb3044
jlanej <jlanej@gmail.com>
2023-02-22 12:07:17 -0600

    :floppy_disk:

--- example/1000G/1000g.example.sh
+++ example/1000G/1000g.example.sh
@@ -51,8 +51,7 @@ seqIndex="$processDir"/1000G_2504_high_coverage.sequence.index
 
 grep -v "#" $seqIndex |cut -f1 \
 |head -n100 \
-| parallel -j1 "echo {.}.cram; \
- singularity run"
+| parallel -j1 "echo {.}.cram; echo $processDir/{/.}"
 
 
 grep -v "#" $seqIndex |cut -f1 \
